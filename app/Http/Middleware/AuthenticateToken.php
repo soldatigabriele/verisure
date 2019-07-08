58abe985d46d2edc34813e7881edea43346cef04
Gabriele Soldati <soldati.gabriele@gmail.com>
2019-07-08 20:30:08 +0100

    added sleep if token is wrong

--- app/Http/Middleware/AuthenticateToken.php
+++ app/Http/Middleware/AuthenticateToken.php
@@ -9,6 +9,7 @@ class AuthenticateToken
     public function handle($request, Closure $next, $guard = null)
     {
         if (config("verisure.auth.active") && ! $request->auth_token == config("verisure.auth.token")) {
+            sleep(0.3);
             abort(401);
         }
         return $next($request);
