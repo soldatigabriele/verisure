17126f5eb5ce26d5c1e02e3fa1a58b81130d898b
Gabriele Soldati <soldati.gabriele@gmail.com>
2019-07-20 13:00:40 +0100

    remove sleep from test

--- app/Http/Middleware/AuthenticateToken.php
+++ app/Http/Middleware/AuthenticateToken.php
@@ -9,7 +9,9 @@ class AuthenticateToken
     public function handle($request, Closure $next, $guard = null)
     {
         if (config("verisure.auth.active") && $request->auth_token !== config("verisure.auth.token")) {
-            sleep(0.3);
+            if (env("APP_ENV") !== "testing"){
+                sleep(0.3);
+            }
             abort(401);
         }
         return $next($request);
