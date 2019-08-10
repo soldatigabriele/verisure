edad410ee164bc4329b4c54b3926e0af00430aed
Gabriele Soldati <soldati.gabriele@gmail.com>
2019-08-10 13:38:36 +0100

    remove logout session call

--- tests/Unit/VerisureClient/VerisureClientLogoutTest.php
+++ tests/Unit/VerisureClient/VerisureClientLogoutTest.php
@@ -24,29 +24,9 @@ class VerisureClientLogoutTest extends TestCase
         $sessionOne = $this->createSession();
         $sessionTwo = $this->createSession();
 
-        $response = new Response(200, []);
-        $guzzleClient = $this->mockGuzzle($response);
-        $client = new VerisureClient($guzzleClient);
-        $client->logout();
+        (new VerisureClient)->logout();
 
         $this->assertNotNull($sessionOne->fresh()->deleted_at);
         $this->assertNotNull($sessionTwo->fresh()->deleted_at);
-    }
-
-    /**
-     * Test an exception is triggered if the response is not 201
-     *
-     * @return void
-     */
-    public function testLogoutFails()
-    {
-        $this->expectException(Exception::class);
-        // Create a valid session
-        $this->createSession();
-
-        $response = new Response(404, []);
-        $guzzleClient = $this->mockGuzzle($response);
-        $client = new VerisureClient($guzzleClient);
-        $client->logout();
     }
 }
